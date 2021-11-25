67f839ab4c0de28b54702e31569b049e2a820429
Márcio Krüger <marcio.kgr@gmail.com>
2021-11-25 16:07:09 -0300

    Add project files.

---
+++ Models/Evento.cs
@@ -0,0 +1,14 @@
+﻿
+namespace EFCoreCrud.Models
+{
+    public class Evento
+    {
+        public Guid Id { get; set; }
+
+        public string Titulo { get; set; }
+
+        public DateTime Data { get; set; } = DateTime.Now;
+
+        public bool Concluido { get; set; }
+    }
+}
