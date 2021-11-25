8732bf3e79fb23389939f08c8e62dbbe3dd1c61d
Márcio Krüger <marcio.kgr@gmail.com>
2021-11-25 17:49:39 -0300

    efetuando insert, select EF
    efetuando insert, select EF

---
+++ Data/MyDataContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using EFCoreCrud.Models;
+
+namespace EFCoreCrud.Data
+{
+    //Contexto de dado do banco de dados
+    public class MyDataContext : DbContext
+    {
+       
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseInMemoryDatabase(databaseName: "banco");
+            options.LogTo(Console.WriteLine); //mostrando sqls sendo executados
+ }
+
+        public DbSet<Evento> Eventos { get; set; }  
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using EFCoreCrud.Data;
+using Microsoft.EntityFrameworkCore;
+using EFCoreCrud.Models;
+
+using (var context = new MyDataContext())
+{
+    //Adicionando evento..
+    var evento = new Evento 
+    { 
+        Titulo = "Teste",
+        Data = DateTime.Now,
+    };
+    context.Eventos.Add(evento);
+
+    var eventoConcluido = new Evento
+    {
+        Titulo = "Teste concluido",
+        Data = DateTime.Now,
+        Concluido = true
+    };
+    context.Eventos.Add(eventoConcluido);
+
+    context.SaveChanges(); //persistindo no banco
+
+    var eventos = context.Eventos
+        .AsNoTracking() //somente leitura, não será utilizado para update e etc..
+        .Where(x => x.Concluido == true)
+        .Select(x => new
+        {
+            Id = x.Id,
+            Titulo = x.Titulo,            
+        })
+        .ToList();
+
+    foreach(var item in eventos)
+    {
+        Console.WriteLine(item.Titulo);
+    }
+}
